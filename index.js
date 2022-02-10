ebf0cc263be97a94ca6c88048911d5b727a26e0c
Ahsan <61945522+ahsan2000@users.noreply.github.com>
2022-02-10 20:58:47 +0500

    added all files of Drums-Kit

---
+++ index.js
@@ -0,0 +1,63 @@
+var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
+
+// make sound by clicking
+for (var i = 0; i < numberOfDrumsButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+// make sound my keyboard
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+// function to create sound on both responses
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play(); 
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play(); 
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play(); 
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play(); 
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play(); 
+            break;
+        default: console.log("Pressing wrong button dude!");
+            break;
+    }
+}
+
+// function for animation on pressed
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    // setting timer to remove class from button
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    } ,100);
+}
